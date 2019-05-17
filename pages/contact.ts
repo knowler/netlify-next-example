111b85b6fae9de34e1a4dfc813952cf17adc196c
Nathan Knowler <nathan@knowler.me>
2019-05-17 02:54:44 -0600

    Add form api hook + manual netlify form submit

--- pages/contact.ts
+++ pages/contact.ts
@@ -2,34 +2,48 @@ import * as React from 'react'
 import { Form, Text, TextArea } from 'informed'
 import { Menu, Button } from '../components'
 
-const Contact: React.FunctionComponent = () => (
-  <>
-    <header pt={4} px={4} pb={2} bg='mirage' color='white'>
-      <Menu />
-    </header>
-    <main p={4} bg='mirage' color='white'>
-      <header>
-        <h1 fontSize={4} fontWeight={300} my={0}>Contact</h1>
+const Contact: React.FunctionComponent = () => {
+  const [formApi, setFormApi] = React.useState({})
+
+  const handleSubmit = event => {
+    fetch('/contact', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...formApi.getValues() }),
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error))
+  }
+
+  return (
+    <>
+      <header pt={4} px={4} pb={2} bg='mirage' color='white'>
+        <Menu />
       </header>
-      <Form name='contact' method='POST' data-netlify='true'>
-        <label display='block' mt={2} mb={3}>
-          <span fontSize={1}>Name</span>
-          <Text display='block' mt={1} field='name' />
-        </label>
-        <label display='block' mt={2} mb={3}>
-          <span fontSize={1}>Email</span>
-          <Text display='block' mt={1} field='email' />
-        </label>
-        <label display='block' my={2}>
-          <span fontSize={1}>Message</span>
-          <TextArea display='block' mt={1} field='message' />
-        </label>
-        <p>
-          <Button type='submit'>Submit</Button>
-        </p>
-      </Form>
-    </main>
-  </>
-)
+      <main p={4} bg='mirage' color='white'>
+        <header>
+          <h1 fontSize={4} fontWeight={300} my={0}>Contact</h1>
+        </header>
+        <Form getApi={setFormApi} onSubmit={handleSubmit}>
+          <label display='block' mt={2} mb={3}>
+            <span fontSize={1}>Name</span>
+            <Text display='block' mt={1} field='name' />
+          </label>
+          <label display='block' mt={2} mb={3}>
+            <span fontSize={1}>Email</span>
+            <Text display='block' mt={1} field='email' />
+          </label>
+          <label display='block' my={2}>
+            <span fontSize={1}>Message</span>
+            <TextArea display='block' mt={1} field='message' />
+          </label>
+          <p>
+            <Button type='submit'>Submit</Button>
+          </p>
+        </Form>
+      </main>
+    </>
+  )
+}
 
 export default Contact
